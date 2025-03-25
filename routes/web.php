cd159448d383b7fbed3894687de430dd3b2e0e05
tPdru <pedroteixeiradeandrade@gmail.com>
2025-03-24 22:06:50 -0300

    views com o HTML feito e rotas nomeadas

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('home');
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.principal');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('fale-conosco');
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.sobre-nos');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('sobre-nos');
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.contato');
